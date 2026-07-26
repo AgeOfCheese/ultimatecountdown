d0a4061ebb33e1e89d698b9c57add9265fdbe60c
Keshav Sumitran <61526291+AgeOfCheese@users.noreply.github.com>
2026-07-25 20:03:34 -0700

    implemented spike box basic

--- countdown/scripts/block_with_spikes.gd
+++ countdown/scripts/block_with_spikes.gd
@@ -1,15 +1,16 @@
 extends NumberedObject
 
-@onready var spike_hitbox = $Area2D
+@onready var spike_hitbox = $SpikeHitbox
 
 func _ready():
 	object_number = 2
+	spike_hitbox.connect("body_entered", _on_area_2d_body_entered)
 
 
 func _on_area_2d_body_entered(body):
 
 	# Leave this empty for now if Player.gd
 	# doesn't have a take_damage() function.
-
-	if body.is_in_group("player"):
+	
+	if body.is_in_group("players"):
 		print("Player touched spikes!")
