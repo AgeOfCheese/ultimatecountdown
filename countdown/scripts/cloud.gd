905576544e8465124d06c219e94c3ddb14de256a
Madhav Krishnan <145315862+MadhavTKrishnan@users.noreply.github.com>
2026-07-25 20:45:31 -0700

    Update cloud.gd

--- countdown/scripts/cloud.gd
+++ countdown/scripts/cloud.gd
@@ -4,3 +4,6 @@ extends NumberedObject
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	object_number = 10
+	
+func apply_effect(player):
+	player.apply_cloud_effect()
