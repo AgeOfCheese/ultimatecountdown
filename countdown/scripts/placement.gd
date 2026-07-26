2085ee62450370132f5e1393325a751779dc6ac1
Prathit Wagholikar <pwagholikar886@student.fuhsd.org>
2026-07-25 20:43:45 -0700

    more placement changes

--- countdown/scripts/placement.gd
+++ countdown/scripts/placement.gd
@@ -1,66 +1,157 @@
 extends Node2D
 
 const GRID_SIZE : int = 14
-const GRID_WIDTH : int = 20   # number of columns to draw (adjust to your level size)
-const GRID_HEIGHT : int = 15  # number of rows to draw
+const BUTTON_SIZE : int = 50
+const TOP_BAR_HEIGHT : int = 60
 
-var current_object : int
-var current_node : Node2D  # the instantiated object currently following the mouse
+var current_object : int = -1
+var current_node : Node2D
+
+var available_objects = [
+	10,9,8,7,6,5,4,3,2,1,0
+]
+
 
 func _ready():
-	get_next_object()
+	pass
 
-func get_next_object():
-	if RoundManager.objects_to_place.is_empty():
-		print("Placement Finished!")
-		RoundManager.end_placement()
-		return
-	current_object = RoundManager.objects_to_place.pop_front()
-	current_node = RoundManager.OBJECT_POOl.get(current_object).instantiate()
-	add_child(current_node)
-	current_node.global_position = get_snapped_mouse_pos()
-	print("Now placing:", current_object)
 
 func _process(_delta):
+
 	if current_node:
 		current_node.global_position = get_snapped_mouse_pos()
 
+
 func get_snapped_mouse_pos() -> Vector2:
+
 	var mouse_pos = get_global_mouse_position()
-	return mouse_pos.snapped(Vector2(GRID_SIZE, GRID_SIZE))
+
+	return mouse_pos.snapped(
+		Vector2(GRID_SIZE, GRID_SIZE)
+	)
+
 
 func _unhandled_input(event):
+
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			place_object()
+
+			var mouse_pos = event.position
+
+			# Check if clicking the top object bar
+			if mouse_pos.y < TOP_BAR_HEIGHT:
+				select_object(mouse_pos)
+			else:
+				place_object()
+
+
+
+func select_object(mouse_pos : Vector2):
+
+	# Don't allow selecting another object while holding one
+	if current_node:
+		print("Already holding an object!")
+		return
+
+	var index = int(mouse_pos.x / BUTTON_SIZE)
+
+	if index >= available_objects.size():
+		return
+
+	current_object = available_objects[index]
+
+	current_node = RoundManager.OBJECT_POOl.get(
+		current_object
+	).instantiate()
+
+	add_child(current_node)
+
+	current_node.global_position = get_snapped_mouse_pos()
+
+	print("Selected object:", current_object)
+
+
 
 func place_object():
+
 	if current_node == null:
 		return
 
 	var grid_pos = get_snapped_mouse_pos()
-	RoundManager.add_object(str(current_object), grid_pos, current_node.rotation)
 
-	print("Placed ", current_object, " at ", grid_pos)
+	RoundManager.add_object(
+		str(current_object),
+		grid_pos,
+		current_node.rotation
+	)
 
-	current_node.global_position = grid_pos
+	print(
+		"Placed ",
+		current_object,
+		" at ",
+		grid_pos
+	)
+
+	# Leave the object in the scene
 	current_node = null
+	current_object = -1
 
-	get_next_object()
+
 
 func _draw():
+
 	var color = Color(1, 1, 1, 0.2)
 
 	var screen_size = get_viewport_rect().size
 
-	# vertical lines
-	for x in range(int(screen_size.x / GRID_SIZE) + 1):
-		var start = Vector2(x * GRID_SIZE, 0)
-		var end = Vector2(x * GRID_SIZE, screen_size.y)
-		draw_line(start, end, color, 1.0)
 
-	# horizontal lines
+	# Grid
+	for x in range(int(screen_size.x / GRID_SIZE) + 1):
+
+		var start = Vector2(x * GRID_SIZE, TOP_BAR_HEIGHT)
+		var end = Vector2(x * GRID_SIZE, screen_size.y)
+
+		draw_line(
+			start,
+			end,
+			color,
+			1.0
+		)
+
+
 	for y in range(int(screen_size.y / GRID_SIZE) + 1):
+
 		var start = Vector2(0, y * GRID_SIZE)
 		var end = Vector2(screen_size.x, y * GRID_SIZE)
-		draw_line(start, end, color, 1.0)
+
+		draw_line(
+			start,
+			end,
+			color,
+			1.0
+		)
+
+
+	# Top object bar
+	for i in range(available_objects.size()):
+
+		var rect = Rect2(
+			i * BUTTON_SIZE,
+			0,
+			BUTTON_SIZE,
+			TOP_BAR_HEIGHT
+		)
+
+		draw_rect(
+			rect,
+			Color(0.2,0.2,0.2,0.8)
+		)
+
+		draw_string(
+			ThemeDB.fallback_font,
+			rect.position + Vector2(18,35),
+			str(available_objects[i]),
+			HORIZONTAL_ALIGNMENT_LEFT,
+			-1,
+			24
+		)
